49d0d6d89960608451ff8cf2de8b042136ac2262
sx399 <sx399@rocketmail.com>
2017-04-18 10:49:43 +0700

    Update menu.js

--- menu.js
+++ menu.js
@@ -12,7 +12,7 @@ var Menu = function (pjs, obj) {
 	var author = obj.author || false;
 	var radius = obj.radius * r / 2 || 0;
 
-	var iHeight = r * 500;
+	var iHeight = r * 5;
 	var iCount  = 0;
 	var iPad = iHeight / 10;
 
